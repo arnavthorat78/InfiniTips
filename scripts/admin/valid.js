9cc3d664f30900de357a21e15cbfce0b13d0d766
arnavthorat78 <62457196+arnavthorat78@users.noreply.github.com>
2021-10-26 08:39:17 +1100

    Added more comments

--- scripts/admin/valid.js
+++ scripts/admin/valid.js
@@ -1,3 +1,4 @@
+// If the user manually went into the admin dashboard, and they weren't logged in, then show them the sign in page.
 if (!sessionStorage.getItem("adminLoggedIn")) {
     location.replace("./sign-in.html");
 }
