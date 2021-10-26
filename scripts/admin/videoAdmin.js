9231ff4ef295ee8dee70521746c7b5585c8a0b2e
arnavthorat78 <62457196+arnavthorat78@users.noreply.github.com>
2021-10-27 08:51:32 +1100

    Added comments for the videos (admin)

--- scripts/admin/videoAdmin.js
+++ scripts/admin/videoAdmin.js
@@ -1,12 +1,22 @@
+// Getting the videos
 const list = document.querySelector(".videos");
 const loadingVideos = document.querySelector(".loadingVideos");
 
+// Getting the elements for adding a video
 const addVideoForm = document.querySelector(".addVideo");
 const feedback = document.querySelector(".feedback");
 
+// Getting the elements for deleting a video
 const deleteFeedback = document.querySelector(".deleteFeedback");
 
+/**
+ * Add a video to the DOM.
+ * 
+ * @param {any} video The data of the video provided by Firestore.
+ * @param {string} id The ID of the video provided by Firestore. This must be unique.
+ */
 const addVideo = (video, id) => {
+	// Setting the HTML template
 	let html = `
         <div class="card mt-3 shadow-sm video" data-id="${id}">
             <div class="card-body">
@@ -41,12 +51,20 @@ const addVideo = (video, id) => {
             </div>
         </div>
     `;
-
+	
+	// Appending the HTML snippet to the start of the video list.
 	list.innerHTML = html + list.innerHTML;
 };
-
+/**
+ * Delete a video from the DOM.
+ * 
+ * @param {string} id The ID of the video to delete.
+ */
 const deleteVideo = (id) => {
+	// Get all of the videos
 	const video = document.querySelectorAll(".video");
+
+	// For each of the videos, if the ID passed in is the ID for deleting, remove it from the DOM.
 	video.forEach((video) => {
 		if (video.getAttribute("data-id") === id) {
 			video.remove();
@@ -54,14 +72,19 @@ const deleteVideo = (id) => {
 	});
 };
 
+// Firestore real-time listener (order by the most recent video at the top).
 db.collection("videos")
 	.orderBy("created_at", "asc")
 	.onSnapshot((snapshot) => {
+		// Cycle through each change
 		snapshot.docChanges().forEach((change) => {
+			// Get the document for each change
 			const doc = change.doc;
 
+			// Remove the loading symbol
 			loadingVideos.classList.add("d-none");
 
+			// Call a method depending on the type
 			if (change.type === "added") {
 				addVideo(doc.data(), doc.id);
 			} else if (change.type === "removed") {
@@ -70,9 +93,12 @@ db.collection("videos")
 		});
 	});
 
+// Add a video
 addVideoForm.addEventListener("submit", (e) => {
+	// Prevent the page from reloading
 	e.preventDefault();
 
+	// Inform the user that the video is being added to the database.
 	feedback.classList.remove("text-success", "text-danger");
 	feedback.classList.add("text-warning");
 	feedback.innerHTML = `
@@ -85,6 +111,8 @@ addVideoForm.addEventListener("submit", (e) => {
         <span>Adding "<i>${addVideoForm.title.value}</i>"!</span>
     `;
 
+	// Set the data as a JavaScript object.
+	// The new Date() is for the timestamp.
 	const now = new Date();
 	const video = {
 		title: addVideoForm.title.value,
@@ -95,9 +123,11 @@ addVideoForm.addEventListener("submit", (e) => {
 		created_at: firebase.firestore.Timestamp.fromDate(now),
 	};
 
+	// Add the video
 	db.collection("videos")
 		.add(video)
 		.then(() => {
+			// Inform the user that the addition was successful.
 			feedback.classList.remove("text-warning");
 			feedback.classList.add("text-success");
 			feedback.innerHTML = `Successfully added video "<i>${addVideoForm.title.value}</i>"!`;
@@ -105,17 +135,22 @@ addVideoForm.addEventListener("submit", (e) => {
 		.catch((err) => {
 			console.log(err);
 
+			// Inform the user that the addition was problematic.
 			feedback.classList.remove("text-warning");
 			feedback.classList.add("text-danger");
 			feedback.innerHTML = `An error occured while uploading "<i>${addVideoForm.title.value}</i>". Please try again later.`;
 		});
 });
 
+// Delete a video
+// Listen for a click inside the list.
 list.addEventListener("click", (e) => {
+	// If the type is a button, and the user confirmed that it should be deleted...
 	if (
 		e.target.getAttribute("data-type") === "BUTTON" &&
 		confirm("Are you sure you want to delete the video? This action is irreversible!")
 	) {
+		// Inform the user that the deletion is in progress.
 		deleteFeedback.classList.remove("text-success", "text-danger");
 		deleteFeedback.classList.add("text-warning");
 		deleteFeedback.innerHTML = `
@@ -128,12 +163,15 @@ list.addEventListener("click", (e) => {
             <span>Deleting video, please wait...</span>
         `;
 
+		// Get the ID to delete
 		const id = e.target.getAttribute("data-id");
 
+		// Delete the document with the ID
 		db.collection("videos")
 			.doc(id)
 			.delete()
 			.then(() => {
+				// Inform the user that the deletion was successful.
 				deleteFeedback.classList.remove("text-warning");
 				deleteFeedback.classList.add("text-success");
 				deleteFeedback.innerHTML = `Successfully deleted video!`;
@@ -141,6 +179,7 @@ list.addEventListener("click", (e) => {
 			.catch((err) => {
 				console.log(err);
 
+				// Inform the user that the deletion was problematic.
 				deleteFeedback.classList.remove("text-warning");
 				deleteFeedback.classList.add("text-danger");
 				deleteFeedback.innerHTML = `An error occured while deleting the video. Please try again later.`;
